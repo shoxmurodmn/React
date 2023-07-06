993a059d3c1906c4f5b6c5d48a4218f0baf8af12
shoxmurodmn <mamatovmurod8898@gmail.com>
2023-07-06 14:15:47 +0500

    reakt component

---
+++ src/mock.js
@@ -0,0 +1,13 @@
+import React from "react";
+let fruit = [
+   { src: `https://img.freepik.com/free-photo/green-apple-with-leaves_1101-453.jpg?size=626&ext=jpg`, name: "Apple" },
+   { src: `https://img.freepik.com/free-vector/realistic-fruits-composition-with-images-whole-sliced-kiwi-fruit-blank-background-vector-illustration_1284-66048.jpg?size=626&ext=jpg&ga=GA1.1.1673455082.1688621806&semt=sph`, name: "kiwi" },
+   { src: `https://t4.ftcdn.net/jpg/00/00/11/53/240_F_115385_2fgiOLeJUny8eKdIbmw68fp013DXdI.jpg`, name: "Banana" },
+   { src: `https://img.freepik.com/free-photo/mandarin-isolated-white-background_93675-131661.jpg?size=626&ext=jpg&ga=GA1.2.1673455082.1688621806&semt=sph`, name: "mandarin" },
+   { src: `https://img.freepik.com/free-vector/realistic-fruits-composition-with-images-whole-sliced-kiwi-fruit-blank-background-vector-illustration_1284-66048.jpg?size=626&ext=jpg&ga=GA1.1.1673455082.1688621806&semt=sph`, name: "kiwi" },
+   { src: `https://img.freepik.com/free-photo/mandarin-isolated-white-background_93675-131661.jpg?size=626&ext=jpg&ga=GA1.2.1673455082.1688621806&semt=sph`, name: "mandarin" },
+   { src: `https://t4.ftcdn.net/jpg/00/00/11/53/240_F_115385_2fgiOLeJUny8eKdIbmw68fp013DXdI.jpg`, name: "Banana" },
+   { src: `https://img.freepik.com/free-photo/green-apple-with-leaves_1101-453.jpg?size=626&ext=jpg`, name: "Apple" },
+]
+
+export default fruit;
